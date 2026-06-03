8edc36cfc5a5c44433d65f73eb9071330b628211
myrqyry <matrmx@gmail.com>
2026-06-03 17:31:58 -0500

    feat: bootstrap Astro 5 project with pnpm

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://myrqyry.github.io',
+  base: '/HELIOS-3D',
+  output: 'static',
+  trailingSlash: 'ignore',
+});
